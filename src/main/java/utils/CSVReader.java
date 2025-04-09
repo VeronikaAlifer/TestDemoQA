2d9d63a60c59ec0d7375dbb503f155cf1735af24
Veranika Alifer <veranika.alifer@gmail.com>
2025-04-09 20:47:15 +0200

    Feat: Add CSWReader and initial CSV file for parsing

--- src/main/java/utils/CSVReader.java
+++ src/main/java/utils/CSVReader.java
@@ -1,2 +1,34 @@
-package utils;public class CSVReader {
+package utils;
+
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+import testdata.Person;
+
+import java.io.FileReader;
+import java.io.Reader;
+import java.util.Collections;
+import java.util.List;
+
+public class CSVReader {
+
+    public static List<Person> readPeopleFromCsv(String path) {
+        return readFromSCV(path, Person.class);
+    }
+
+    private static <T> List<T> readFromSCV(String path, Class<T> type) {
+        try (Reader reader = new FileReader(path)) {
+            // Чтение CSV файла и маппинг данных в объекты Person
+            CsvToBean<T> csvToBean = new CsvToBeanBuilder<T>(reader)
+                    .withType(type)
+                    .withIgnoreLeadingWhiteSpace(true) // Игнорирование пробелов в начале данных
+                    .build();
+
+            return csvToBean.parse();  // Получаем список объектов Person
+
+        } catch (Exception e) {
+            System.err.println("⚠ Ошибка при чтении CSV: " + path + ", тип: " + type.getSimpleName());
+        }
+        return Collections.emptyList();
+    }
 }
+
